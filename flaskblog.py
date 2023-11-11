aa53f5b1a53bca48d39dac45990525c8536af93a
talhaejaz539 <talhaejazup@gmail.com>
2023-11-11 10:41:02 +0500

    Module 2 Completed

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Talha Ejaz',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 25, 2023'
+    },
+    {
+        'author': 'Muhammad Ejaz',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 25, 2023'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
